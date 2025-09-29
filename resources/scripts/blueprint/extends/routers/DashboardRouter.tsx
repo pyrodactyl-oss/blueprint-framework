44654b34978674d9b94ff3ad0056904fb8983580
Ryder <719coolboy@gmail.com>
2025-09-29 03:51:20 -0600

    Reworks Routers to follow new API change from react-router-dom

--- resources/scripts/blueprint/extends/routers/DashboardRouter.tsx
+++ resources/scripts/blueprint/extends/routers/DashboardRouter.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { NavLink, Route, Switch } from 'react-router-dom';
-import { useLocation } from 'react-router';
+import { NavLink, Route, Routes, useLocation } from 'react-router-dom';
 import { NotFound } from '@/components/elements/ScreenBlock';
-import TransitionRouter from '@/TransitionRouter';
 import DashboardContainer from '@/components/dashboard/DashboardContainer';
 import Spinner from '@/components/elements/Spinner';
 import { useStoreState } from 'easy-peasy';
@@ -12,74 +10,72 @@ import blueprintRoutes from './routes';
 import { UiBadge } from '@blueprint/ui';
 
 export const NavigationLinks = () => {
-  const rootAdmin = useStoreState((state) => state.user.data!.rootAdmin);
-  return (
-    <>
-      {/* Pterodactyl routes */}
-      {routes.account
-        .filter((route) => !!route.name)
-        .map(({ path, name, exact = false }) => (
-          <NavLink key={path} to={`/account/${path}`.replace('//', '/')} exact={exact}>
-            {name}
-          </NavLink>
-        ))}
-
-      {/* Blueprint routes */}
-      {blueprintRoutes.account.length > 0 &&
-        blueprintRoutes.account
-          .filter((route) => !!route.name)
-          .filter((route) => (route.adminOnly ? rootAdmin : true))
-          .map(({ path, name, exact = false, adminOnly }) => (
-            <NavLink key={path} to={`/account/${path}`.replace('//', '/')} exact={exact}>
-              {name}
-              {adminOnly ? (
-                <>
-                  <span className={'hidden'}>(</span>
-                  <UiBadge>ADMIN</UiBadge>
-                  <span className={'hidden'}>)</span>
-                </>
-              ) : undefined}
-            </NavLink>
-          ))}
-    </>
-  );
-};
-
-export const NavigationRouter = () => {
-  const location = useLocation();
-  const rootAdmin = useStoreState((state) => state.user.data!.rootAdmin);
-  return (
-    <>
-      <TransitionRouter>
-        <React.Suspense fallback={<Spinner centered />}>
-          <Switch location={location}>
-            <Route path={'/'} exact>
-              <DashboardContainer />
-            </Route>
-
+    const rootAdmin = useStoreState((state) => state.user.data!.rootAdmin);
+    return (
+        <>
             {/* Pterodactyl routes */}
-            {routes.account.map(({ path, component: Component }) => (
-              <Route key={path} path={`/account/${path}`.replace('//', '/')} exact>
-                <Component />
-              </Route>
-            ))}
+            {routes.account
+                .filter((route) => !!route.name)
+                .map(({ path, name, exact = false }) => (
+                    <NavLink key={path} to={`/account/${path}`.replace('//', '/')} end={!!exact}>
+                        {name}
+                    </NavLink>
+                ))}
 
             {/* Blueprint routes */}
             {blueprintRoutes.account.length > 0 &&
-              blueprintRoutes.account
-                .filter((route) => (route.adminOnly ? rootAdmin : true))
-                .map(({ path, component: Component }) => (
-                  <Route key={path} path={`/account/${path}`.replace('//', '/')} exact>
-                    <Component />
-                  </Route>
-                ))}
+                blueprintRoutes.account
+                    .filter((route) => !!route.name)
+                    .filter((route) => (route.adminOnly ? rootAdmin : true))
+                    .map(({ path, name, exact = false, adminOnly }) => (
+                        <NavLink key={path} to={`/account/${path}`.replace('//', '/')} end={!!exact}>
+                            {name}
+                            {adminOnly ? (
+                                <>
+                                    <span className={'hidden'}>(</span>
+                                    <UiBadge>ADMIN</UiBadge>
+                                    <span className={'hidden'}>)</span>
+                                </>
+                            ) : undefined}
+                        </NavLink>
+                    ))}
+        </>
+    );
+};
 
-            <Route path={'*'}>
-              <NotFound />
-            </Route>
-          </Switch>
-        </React.Suspense>
-      </TransitionRouter>
-    </>
-  );
+export const NavigationRouter = () => {
+    const location = useLocation();
+    const rootAdmin = useStoreState((state) => state.user.data!.rootAdmin);
+    return (
+        <>
+            <React.Suspense fallback={<Spinner centered />}>
+                <Routes location={location}>
+                    <Route path="/" element={<DashboardContainer />} />
+
+                    {/* Pterodactyl routes */}
+                    {routes.account.map(({ path, component: Component }) => (
+                        <Route
+                            key={path}
+                            path={`/account/${path}`.replace('//', '/')}
+                            element={<Component />}
+                        />
+                    ))}
+
+                    {/* Blueprint routes */}
+                    {blueprintRoutes.account.length > 0 &&
+                        blueprintRoutes.account
+                            .filter((route) => (route.adminOnly ? rootAdmin : true))
+                            .map(({ path, component: Component }) => (
+                                <Route
+                                    key={path}
+                                    path={`/account/${path}`.replace('//', '/')}
+                                    element={<Component />}
+                                />
+                            ))}
+
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </React.Suspense>
+        </>
+    );
 };
